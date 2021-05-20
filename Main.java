4e6b5832336af5edc05329ee04aff1997600013f
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-05-20 00:10:01 -0500

    Basic

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Subs {
+
+    public static void main(String[] args) {
+	// write your code here
+       
+        long v= 3220000000000000L;
+        float g= 3.14F;
+        double s= 6.54455339394934943;
+
+        System.out.println("Hello world.");
+        System.out.println(v);
+        System.out.println(g);
+        System.out.println(s);
+    }
+}
